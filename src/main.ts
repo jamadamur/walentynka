6064d6848a3e4b14cda12efd874b7753f7569bb1
Jamadamur <ancuent@gmail.com>
2026-02-05 11:12:23 +0100

    add styles and animation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
   <div>
     <div class="card">
-    <div>Would you be my Valentine?</div>
-      <button type="button">Yes</button>
-      <button type="button" disabled>No</button>
+      <div class="text-content">
+        <div class="name">Maryna</div>
+        <div class="question">Would you be my Valentine?</div>
+      </div>
+      <div class="button-group">
+        <button id="yes-btn" type="button">Yes</button>
+        <button type="button" disabled>No</button>
+      </div>
     </div>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+document.querySelector<HTMLButtonElement>('#yes-btn')?.addEventListener('click', () => {
+    app.innerHTML = `
+      <div class="card">
+        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGcxdWF5cnNocnd5NTBvazc1NDk4YmZqbHVlN2N1M2FxdjZjODRvMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/HJibfnd7xqk5hAMD4v/giphy.gif" alt="Success" style="max-width: 100%; height: auto;" />
+      </div>
+    `
+})
